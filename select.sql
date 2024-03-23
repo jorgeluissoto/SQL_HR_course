afc3dd6ccf7e53fc7f15c67c35555c680ecc4bf9
Jorge Soto <jls@jorgeluissoto.com>
2024-03-22 23:02:29 -0400

    first commit

---
+++ select.sql
@@ -0,0 +1,13 @@
+-- Select all data from the “departments” table.
+SELECT 
+    *
+FROM
+    departments;
+    
+-- Select all people from the “employees” table whose first name is “Elvis”.
+SELECT 
+    *
+FROM
+    employees
+WHERE
+    first_name = 'Elvis';
