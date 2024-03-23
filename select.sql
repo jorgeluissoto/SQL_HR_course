8a02d39dd1ffc58217cdd00ebca282450a6bdf18
Jorge Soto <jls@jorgeluissoto.com>
2024-03-23 13:13:34 -0400

    continuing the select section of the course

--- select.sql
+++ select.sql
@@ -11,3 +11,32 @@ FROM
     employees
 WHERE
     first_name = 'Elvis';
+
+-- Retrieve a list with all female employees whose first name is Kellie.
+SELECT 
+    *
+FROM
+    employees
+WHERE
+    first_name = 'Kellie' AND gender = 'F';
+
+-- Retrieve a list with all employees whose first name is either Kellie or Aruna.
+SELECT 
+    *
+FROM
+    employees
+WHERE
+    first_name = 'Kellie'
+        OR first_name = 'Aruna';
+
+-- Retrieve a list with all female employees whose first name is either Kellie or Aruna.
+SELECT 
+    *
+FROM
+    employees
+WHERE
+    gender = 'F'
+        AND (first_name = 'Kellie'
+        OR first_name = 'Aruna');
+    
+    
